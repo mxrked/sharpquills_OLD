8c2a724a25da6ac1ae03ec0364fb5c701cb9e8cc
Parker <pp101socials@gmail.com>
2023-12-06 10:11:23 -0500

    Created file

---
+++ assets/functions/data/cart/KeepCartItems.js
@@ -0,0 +1,13 @@
+/**
+ *
+ *  This is the function that will keep the cart items
+ *
+ */
+
+import {
+  USER_CART_ITEMS,
+  USER_CART_QUANTITIES,
+  USER_CART_SUBTOTALS,
+} from "@/assets/data/variables/ARRAYS";
+
+export default function KeepCartItems() {}
