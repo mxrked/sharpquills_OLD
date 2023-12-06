1b290e18195d505359b54b15918f0480eb29505a
Parker <pp101socials@gmail.com>
2023-12-06 10:00:03 -0500

    Added arrays

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -1,1 +1,7 @@
 // This file is used to hold some arrays
+
+const USER_CART_ITEMS = [];
+const USER_CART_QUANTITIES = [];
+const USER_CART_SUBTOTALS = [];
+
+export { USER_CART_ITEMS, USER_CART_QUANTITIES, USER_CART_SUBTOTALS };
