659bcf8eb05ee7e53772f840e899064d0bec9da9
Parker <pp101socials@gmail.com>
2023-12-08 06:55:14 -0500

    Added data and components

--- pages/cart.js
+++ pages/cart.js
@@ -11,14 +11,26 @@ import DeclareStorageVariable from "@/assets/functions/data/storage/DeclareStora
 import SaveCartItems from "@/assets/functions/data/cart/SaveCartItems";
 
 // Component Imports
+import { PageHead } from "@/assets/components/global/All/PageHead";
 
 // Style Imports
 import "../assets/styles/modules/Cart/Cart.module.css";
 
 export async function getServerSideProps() {
+  const PAGE_HEAD_DATA_DIRECTORY = "public/data/Page_Head_Data/";
   const STORE_DATA_DIRECTORY = "public/data/Store_Data/";
   const UTF8 = "utf-8";
 
+  const PH_ICONS_DATA_FP = path.join(
+    process.cwd(),
+    PAGE_HEAD_DATA_DIRECTORY,
+    "PH_Icons.json"
+  );
+  const PH_DATA_FP = path.join(
+    process.cwd(),
+    PAGE_HEAD_DATA_DIRECTORY,
+    "PH_Cart.json"
+  );
   const FOOD_ITEMS_DATA_FP = path.join(
     process.cwd(),
     STORE_DATA_DIRECTORY,
@@ -35,21 +47,29 @@ export async function getServerSideProps() {
     "Housing.json"
   );
 
+  const PH_ICONS_DATA_FC = fs.readFileSync(PH_ICONS_DATA_FP, UTF8);
+  const PH_DATA_FC = fs.readFileSync(PH_DATA_FP, UTF8);
   const FOOD_ITEMS_DATA_FC = fs.readFileSync(FOOD_ITEMS_DATA_FP, UTF8);
   const TOY_ITEMS_DATA_FC = fs.readFileSync(TOY_ITEMS_DATA_FP, UTF8);
   const HOUSING_ITEMS_DATA_FC = fs.readFileSync(HOUSING_ITEMS_DATA_FP, UTF8);
 
+  let PH_ICONS_DATA = undefined;
+  let PH_DATA = undefined;
   let FOOD_ITEMS_DATA = undefined;
   let TOY_ITEMS_DATA = undefined;
   let HOUSING_ITEMS_DATA = undefined;
 
   try {
+    PH_ICONS_DATA = JSON.parse(PH_ICONS_DATA_FC);
+    PH_DATA = JSON.parse(PH_DATA_FC);
     FOOD_ITEMS_DATA = JSON.parse(FOOD_ITEMS_DATA_FC);
     TOY_ITEMS_DATA = JSON.parse(TOY_ITEMS_DATA_FC);
     HOUSING_ITEMS_DATA = JSON.parse(HOUSING_ITEMS_DATA_FC);
 
     return {
       props: {
+        PH_ICONS_DATA,
+        PH_DATA,
         FOOD_ITEMS_DATA,
         TOY_ITEMS_DATA,
         HOUSING_ITEMS_DATA,
@@ -60,6 +80,8 @@ export async function getServerSideProps() {
 
     return {
       props: {
+        PH_ICONS_DATA: null,
+        PH_DATA: null,
         FOOD_ITEMS_DATA: null,
         TOY_ITEMS_DATA: null,
         HOUSING_ITEMS_DATA: null,
@@ -69,6 +91,8 @@ export async function getServerSideProps() {
 }
 
 export default function Cart({
+  PH_ICONS_DATA,
+  PH_DATA,
   FOOD_ITEMS_DATA,
   TOY_ITEMS_DATA,
   HOUSING_ITEMS_DATA,
@@ -89,6 +113,8 @@ export default function Cart({
 
   return (
     <div id="PAGE" className="page half-second">
+      <PageHead page_head_data={PH_DATA} icons_data={PH_ICONS_DATA} />
+
       <div id="PAGE_CNT"></div>
     </div>
   );
