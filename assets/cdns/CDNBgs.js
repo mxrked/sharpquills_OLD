57b64080b18950e56706713574088d17ff01dfca
Parker <pp101socials@gmail.com>
2023-12-19 16:13:48 -0500

    Added cdn

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -7,5 +7,7 @@
 import { CDNBGReturn } from "./CDNReturns";
 
 const INDEX_TOP_BG = CDNBGReturn("index", "top-bg", "webp");
+const INDEX_STORE_BG = CDNBGReturn("index", "store-bg", "webp");
+const INDEX_INFO_BG = CDNBGReturn("index", "info-bg", "webp");
 
-export { INDEX_TOP_BG };
+export { INDEX_TOP_BG, INDEX_STORE_BG, INDEX_INFO_BG };
