e6578f30e7b6c1be969b5342d780ae81ca5660c7
Parker <pp101socials@gmail.com>
2023-12-21 08:44:42 -0500

    Created file

---
+++ assets/components/pages/All/TextWithLinks.js
@@ -0,0 +1,50 @@
+/**
+ *
+ *  This is the TextWithLinks
+ *
+ */
+
+export const TextWithLinks = (props) => {
+  const OBJECT = props.object;
+  const HEADING_TEXT = OBJECT.headingText;
+  const MAIN_TEXT = OBJECT.mainText;
+  const LINKS = OBJECT.links;
+  const STYLES = OBJECT.stylesSrc;
+
+  return (
+    <div id={OBJECT.id} className={`${STYLES.text_with_links}`}>
+      {HEADING_TEXT !== "" &&
+      HEADING_TEXT !== undefined &&
+      HEADING_TEXT !== null ? (
+        <h2
+          className={`${STYLES.heading_text} orientation-change-element half-second`}
+        >
+          {HEADING_TEXT}
+        </h2>
+      ) : null}
+
+      {MAIN_TEXT !== "" && MAIN_TEXT !== undefined && MAIN_TEXT !== null ? (
+        <p
+          className={`${STYLES.main_text} orientation-change-element half-second`}
+        >
+          {MAIN_TEXT}
+        </p>
+      ) : null}
+
+      {LINKS.length > 0 ? (
+        <ul>
+          {LINKS.map((link) => {
+            <li key={link.linkID}>
+              <a
+                href={link.linkRoute}
+                className="orientation-change-element half-second"
+              >
+                <span>{link.linkName}</span>
+              </a>
+            </li>;
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
