c3efd2c6368a04229e2a1e6866ca9c5fdf8e4521
Parker <pp101socials@gmail.com>
2023-12-21 08:44:06 -0500

    Added bgs

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -9,5 +9,6 @@ import { CDNBGReturn } from "./CDNReturns";
 const INDEX_TOP_BG = CDNBGReturn("index", "top-bg", "webp");
 const INDEX_STORE_BG = CDNBGReturn("index", "store-bg", "webp");
 const INDEX_INFO_BG = CDNBGReturn("index", "info-bg", "webp");
+const INDEX_CONTACT_BG = CDNBGReturn("index", "contact-bg", "webp");
 
-export { INDEX_TOP_BG, INDEX_STORE_BG, INDEX_INFO_BG };
+export { INDEX_TOP_BG, INDEX_STORE_BG, INDEX_INFO_BG, INDEX_CONTACT_BG };
