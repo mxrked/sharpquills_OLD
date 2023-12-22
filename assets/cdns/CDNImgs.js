be30ba99d0b936cab5d585807b2f1ee4b49579c8
Parker <pp101socials@gmail.com>
2023-12-22 10:01:28 -0500

    Added cdns

--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -8,4 +8,25 @@ import { CDNImgReturn } from "./CDNReturns";
 
 const WHITE_WAVE = CDNImgReturn("svgs", "white-wave-1", "svg");
 
+const ATELERIX_TYPE = CDNImgReturn("atelerix", "atelerix-type", "webp");
+const ERINACEUS_TYPE = CDNImgReturn("erinaceus", "erinaceus-type", "webp");
+const HEMIECHINUS_TYPE = CDNImgReturn(
+  "hemiechinus",
+  "hemiechinus-type",
+  "webp"
+);
+const MESECHINUS_TYPE = CDNImgReturn("mesechinus", "mesechinus-type", "webp");
+const PARAECHINUS_TYPE = CDNImgReturn(
+  "paraechinus",
+  "paraechinus-type",
+  "webp"
+);
+
 export { WHITE_WAVE };
+export {
+  ATELERIX_TYPE,
+  ERINACEUS_TYPE,
+  HEMIECHINUS_TYPE,
+  MESECHINUS_TYPE,
+  PARAECHINUS_TYPE,
+};
